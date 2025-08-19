e8a4152e15ac915a71eae968df7dea97052e8b3f
Nathan Onraet <nathan.onraet@ugent.be>
2025-08-19 22:28:38 +0200

    First Commit

---
+++ src/main/kotlin/com/zeus/zodom/ZodomApplication.kt
@@ -0,0 +1,11 @@
+package com.zeus.zodom
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZodomApplication
+
+fun main(args: Array<String>) {
+	runApplication<ZodomApplication>(*args)
+}
---
+++ src/test/kotlin/com/zeus/zodom/ZodomApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zeus.zodom
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZodomApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
