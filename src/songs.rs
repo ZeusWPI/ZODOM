8d1316ac55b336d14553a2e8ce55f9034fc6d30d
Nathan Onraet <Nathan.Onraet@UGent.Be>
2025-08-28 03:40:49 +0200

    Did Some More Error Handling

--- src/songs.rs
+++ src/songs.rs
@@ -58,20 +58,14 @@ pub fn init_client() -> paho_mqtt::Client {
         .client_id(MQTT_CLIENT_ID.to_string())
         .finalize();
 
-    let cli = paho_mqtt::Client::new(create_opts).unwrap_or_else(|err| {
-        println!("Error creating the MQTT client: {:?}", err);
-        process::exit(1);
-    });
+    let cli = paho_mqtt::Client::new(create_opts).expect("Error creating the MQTT client");
 
     let conn_opts = ConnectOptionsBuilder::new()
         .keep_alive_interval(Duration::from_secs(20))
         .clean_session(false)
         .finalize();
 
-    if let Err(e) = cli.connect(conn_opts) {
-        println!("Unable to connect to MQTT server:\n\t{:?}", e);
-        process::exit(1);
-    }
+    cli.connect(conn_opts).expect("Unable to connect to MQTT server");
 
     cli
 }
@@ -83,30 +77,31 @@ async fn listen(client: Arc<paho_mqtt::Client>, last_song: Arc<Mutex<SongInfo>>,
     for msg in queue.iter() {
         if let Some(msg) = msg {
             println!("Received Msg: {}", msg);
-            if msg.topic() == "music/current_song_info" {
-                let payload: GuitarSongInfo = serde_json::from_str(&*msg.payload_str()).unwrap();
-
-                update_songs(last_song.clone(), current_song.clone(), SongInfo {
-                    title: payload.name,
-                    artist: payload.artists.join(", "),
-                    cover_img: payload.image_url.unwrap_or(String::from("/static/assets/cover-placeholder.svg")),
-                    song_id: payload.spotify_id,
-                    paused_on: SystemTime::UNIX_EPOCH,
-                }).await
-            } else if msg.topic() == "music/events/paused" || msg.topic() == "music/events/stopped" {
-                pause_song(last_song.clone(), current_song.clone()).await;
+            match msg.topic() {
+                "music/current_song_info" => {
+                    if let Ok(payload) = serde_json::from_str::<GuitarSongInfo>(&*msg.payload_str()) {
+                        update_songs(last_song.clone(), current_song.clone(), SongInfo {
+                            title: payload.name,
+                            artist: payload.artists.join(", "),
+                            cover_img: payload.image_url.unwrap_or(String::from("/static/assets/cover-placeholder.svg")),
+                            song_id: payload.spotify_id,
+                            paused_on: SystemTime::UNIX_EPOCH,
+                        }).await
+                    }
+                }
+                "music/events/paused" | "music/events/stopped" => { pause_song(last_song.clone(), current_song.clone()).await; }
+                _ => {}
             }
-        } else if !client.is_connected() {
-            if try_reconnect(&client) {
-                subscribe_topics(&client)
-            }
+        } else if !client.is_connected() && try_reconnect(&client) {
+            subscribe_topics(&client)
         }
     }
 }
 
+
 fn subscribe_topics(client: &paho_mqtt::Client) {
     let topics = &["music/events/paused", "music/events/stopped", "music/current_song_info"];
-    client.subscribe_many_same_qos(topics, QOS_2).unwrap();
+    client.subscribe_many_same_qos(topics, QOS_2).expect("Failed To Subscribe to MQTT Topics");
 }
 
 pub fn start_listening(client: Arc<paho_mqtt::Client>, last_song: Arc<Mutex<SongInfo>>, current_song: Arc<Mutex<SongInfo>>) {
@@ -120,7 +115,7 @@ pub fn publish_vote_update(client: &paho_mqtt::Client, vote_count: VoteCount) {
         serde_json::to_string(&vote_count).unwrap(),
         QoS::ExactlyOnce,
     );
-    client.publish(msg).unwrap();
+    client.publish(msg).unwrap_or_else(|_| { println!("Couldn't Publish Vote Update") }); //TODO Change To tracing library
 }
 
 async fn update_songs(last_song: Arc<Mutex<SongInfo>>, current_song: Arc<Mutex<SongInfo>>, new_song: SongInfo) {
